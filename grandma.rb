b6cc627428628858ee2a3dcb30eb7ecdf385e3d9
Yu Li <yli0607x@gmail.com>
2018-10-03 20:39:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,8 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase()
   return "HUH?! SPEAK UP, SONNY!"
+  if phrase = "I LOVE YOU GRANDMA!"
+  return "I LOVE YOU TOO PUMPKIN!"
 else
   return "NO, NOT SINCE 1938!"
 end
